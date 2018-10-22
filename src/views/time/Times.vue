6265d06812685d8f6ddc1dcbb439913283e8175d
Daniel Fegan <daniel.fegan@moneysupermarket.com>
2018-10-22 09:27:49 +0100

    output message is server unavailable

--- src/views/time/Times.vue
+++ src/views/time/Times.vue
@@ -1,15 +1,23 @@
 <template>
   <v-layout ma-5 row fill-height>
-    <v-flex xs12 md6 lg6>
-      <v-toolbar dark color="grey darken-2">
-        <v-toolbar-title>{{title}}</v-toolbar-title>
-      </v-toolbar>
-      <v-responsive class="white elevation-6 px-3 pt-3 pb-3">
-        <strong class="pa-2">Local Time: {{times.localTime}}</strong>
-        <br>
-        <strong class="pa-2">Time in Canada: {{times.canadaTime}}</strong>
-      </v-responsive>
-    </v-flex>
+      <v-flex xs12 md6 lg6 v-if="times">
+        <v-toolbar dark color="grey darken-2">
+          <v-toolbar-title>{{title}}</v-toolbar-title>
+        </v-toolbar>
+        <v-responsive class="white elevation-6 px-3 pt-3 pb-3">
+          <strong class="pa-2">Local Time: {{times.localTime}}</strong>
+          <br>
+          <strong class="pa-2">Time in Canada: {{times.canadaTime}}</strong>
+        </v-responsive>
+      </v-flex>
+      <v-flex xs12 md6 lg6 v-else>
+        <v-toolbar dark color="grey darken-2">
+          <v-toolbar-title>{{title}}</v-toolbar-title>
+        </v-toolbar>
+        <v-responsive class="white elevation-6 px-3 pt-3 pb-3">
+          <strong class="red--text">Server currently unavailable</strong>
+        </v-responsive>
+      </v-flex>
   </v-layout>
 </template>
 
@@ -19,7 +27,7 @@ export default {
   data() {
     return {
       title:  'Current Times',
-      times: ''
+      times: null
     }
   },
   async mounted() {
